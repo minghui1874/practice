5c63513d863f0692200f68a7a06e6d87425d1297
明辉 <minghui@thunisoft.com>
2017-03-06 10:37:17 +0800

    add more

--- src/com/thunisoft/fibonacci/FibonacciTask.java
+++ src/com/thunisoft/fibonacci/FibonacciTask.java
@@ -28,7 +28,17 @@ public class FibonacciTask extends RecursiveTask<Integer>{
             fibo = (end -1)+(end -2);
         }else{
             //如果任务大于阀值，就分裂为两个子任务来计算
+            int mid = (end - start)/2;
+            FibonacciTask t1 = new FibonacciTask(start, mid);
+            FibonacciTask t2 = new FibonacciTask(mid, end);
+            //执行子任务
+            t1.fork();
+            t2.fork();
+            //等待子任务执行完成，并得到其结果
+            int t1Result = t1.join();
+            int t2Result = t2.join();
+            fibo = t1Result + t2Result;
         }
-        return null;
+        return fibo;
     }
 }
---
+++ src/com/thunisoft/fibonacci/Main.java
@@ -0,0 +1,18 @@
+package com.thunisoft.fibonacci;
+
+import java.util.Scanner;
+import java.util.concurrent.ForkJoinPool;
+
+/**
+ * @author minghui
+ * @create 2017-03-03 18:02
+ */
+public class Main {
+    public static void main(String[] args){
+        ForkJoinPool pool = new ForkJoinPool();
+        Scanner scan = new Scanner(System.in);
+        int end = Integer.valueOf(scan.next());
+        FibonacciTask fibo = new FibonacciTask(0, end);
+        
+    }
+}
