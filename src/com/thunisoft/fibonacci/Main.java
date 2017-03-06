b899f7b6a7157ee92a82a13fb7054d63595b9fcb
明辉 <minghui@thunisoft.com>
2017-03-06 14:04:45 +0800

    关闭ForkJoinPool

--- src/com/thunisoft/fibonacci/Main.java
+++ src/com/thunisoft/fibonacci/Main.java
@@ -23,6 +23,8 @@ public class Main {
                System.out.println("当前位数结果为:"+result.get());
            }catch(InterruptedException  | ExecutionException e){
                e.printStackTrace();
+           }finally{
+               pool.shutdown();
            }
        }
 
